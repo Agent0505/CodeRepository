46690a1a57629df3ae8c640e98849b7b2cb3f209
Agent0505 <40176253+Agent0505@users.noreply.github.com>
2021-07-01 09:14:56 +0300

    Prepare motor speed fix

--- WORK/Core/Src/main.c
+++ WORK/Core/Src/main.c
@@ -463,7 +463,7 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 240;
+  htim3.Init.Prescaler = 150;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 116;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV4;
